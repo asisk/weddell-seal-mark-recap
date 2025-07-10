926b6ba1aba26285da317cedb8183a7971837dba
asisk <annie@ignistech.io>
2025-07-09 21:25:41 -0600

    -cleanup

--- AndroidStudioProjects/WeddellSealMarkRecap/app/src/main/java/weddellseal/markrecap/ui/ObservationItem.kt
+++ AndroidStudioProjects/WeddellSealMarkRecap/app/src/main/java/weddellseal/markrecap/ui/ObservationItem.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.DropdownMenu
